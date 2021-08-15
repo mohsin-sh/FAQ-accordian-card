45c2751b56dd8f2cf5393493d98fd218bff86e80
Mohsin <mohsinshah60752@gmail.com>
2021-08-16 00:13:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function answer1(){
+    document.getElementById("question1").innerHTML = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+   
+}
+
+function answer2(){
+    document.getElementById("question2").innerHTML = "No more than 2GB. All files in your account must fit your allotted storage space."
+}
+function answer3(){
+    document.getElementById("question3").innerHTML = " Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+}
+
+function answer4(){
+    document.getElementById("question4").innerHTML = "Yes! Send us a message and we’ll process your request no questions asked."
+}
+function answer5(){
+    document.getElementById("question5").innerHTML = "Chat and email support is available 24/7. Phone lines are open during normal business hours"
+}
+
+
+
+
+
+
+
+
+
+
+
